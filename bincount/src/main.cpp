68c3b6e1b26798f5f1a005bc1bb929cb136cc410
Bruno Augusto <bruno_agst@hotmail.com>
2023-03-22 19:56:05 -0300

    implement count

---
+++ bincount/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#define RED PB4
+#define WHITE PB5
+#define GREEN PB6
+#define YELLOW PB7
+#define PUSHBUTTON PINH6
+
+int count = 4;
+void setup() {
+  // configure pins
+  DDRB |= ((1<<DDB4) | (1<<DDB5) | (1<<DDB6) | (1<<DDB7)); //configure pins to output
+  PORTB &= ~((1<<RED) | (1<<WHITE) | (1<<GREEN) | (1<<YELLOW)); //inicialize pins with 0
+  DDRH |= (0<<DDH6); //configure pushbutton
+  PORTH |= (1<<PH6); //configure pullup
+}
+
+void loop() {
+  if((PINH & (1<<PUSHBUTTON)) == 0){
+    PORTB |= (1<<count);
+    count++;
+    if(count > 8){ //reset count
+      PORTB &= ~((1<<RED) | (1<<WHITE) | (1<<GREEN) | (1<<YELLOW));
+      count = 4;
+    }
+    delay(300);
+  }
+}
+   
